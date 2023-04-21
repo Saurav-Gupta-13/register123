8d7e4f08778db189fbc7a22b0e86a843feab1d06
Saurav Gupta <91198524+Saurav-Gupta-13@users.noreply.github.com>
2023-04-21 18:39:07 +0530

    RegisterPage.jsx

--- src/RegisterPage.jsx
+++ src/RegisterPage.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { DataStore } from '@aws-amplify/datastore';
-import { Register } from './models';
 
 function RegisterPage() {
   const [email, setEmail] = useState("");
@@ -19,22 +17,10 @@ function RegisterPage() {
     setConfirmPassword(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // Do something with the form data, such as submitting it to a server\
-    try {
-      await DataStore.save(
-        new Register({
-          "email": email,
-          "password": password
-        })
-      );
-      console.log("Registration successful!");
-    } catch (error) {
-      console.error("Error saving registration data:", error);
-    }
+    // Do something with the form data, such as submitting it to a server
   };
-  
 
   return (
     <div>
@@ -70,7 +56,72 @@ function RegisterPage() {
             required
           />
         </div>
-        <button type="submit" onClick={handleSubmit}>Register</button>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterPage;
+import React, { useState } from "react";
+
+function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Do something with the form data, such as submitting it to a server
+  };
+
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
