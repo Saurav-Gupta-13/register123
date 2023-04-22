bf8d248dbfdf9648aa3f9329d488e26bcb6d8308
Saurav <sauravmgupta13@gmail.com>
2023-04-22 12:02:32 +0530

    updated page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import {awsConfig} from './config/aws-exports';
+import {awsConfig} from './aws-exports';
 
 Amplify.configure(awsConfig);
 
